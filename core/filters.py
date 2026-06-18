f74211172e84806130cfff53a03c65e2224d7b25
Yash Rathi <167305367+YashRathi1035@users.noreply.github.com>
2026-06-18 20:29:59 +0530

    Implement filter processing workflow

---
+++ core/filters.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+class ImageFilters:
+    def gaussian(image):
+        return cv2.GaussianBlur(image, (7,7), 0)
+    
+    def laplacian(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        lap = cv2.Laplacian(gray, cv2.CV_64F)
+
+        return np.uint8(np.absolute(lap))
+    
+    def canny(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        canny = cv2.Canny(gray, 100, 200)
+        return canny
+    
+    def median(image):
+        return cv2.medianBlur(image, 5)
+    
+    def sobel(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        sobelx = np.uint8(np.absolute(sobelx))
+        sobely = np.uint8(np.absolute(sobely))
+        sobel = cv2.bitwise_or(sobelx, sobely)
+        return sobel
+    
+    def blur(image):
+        return cv2.blur(image, (7, 7))
+    
+    def bilateral(image):
+        return cv2.bilateralFilter(image, 9, 75, 75)
+    
+    def grayscale(image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -1,13 +1,19 @@
 from PySide6.QtWidgets import (QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QLabel,
                                QLineEdit, QWidget, QFileDialog)
+from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtCore import Qt
 from ui.menubar import MenuBar
 from ui.toolbar import ToolBar
 from ui.sidebar import SideBar
+from core.filters import ImageFilters
+import cv2
+import numpy as np
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.original_image = None
+        self.processed_image = None
         self.setWindowTitle('Image Processing Studio')
         self.resize(1400,800)
         self.setup_ui()
@@ -38,8 +44,16 @@ class MainWindow(QMainWindow):
 
         ## Sidebar
         self.sidebar = SideBar()
-        main_layout.addWidget(self.sidebar)
+        self.sidebar.gaussian.clicked.connect(lambda : self.apply_filters(self.sidebar.gaussian))
+        self.sidebar.laplacian.clicked.connect(lambda : self.apply_filters(self.sidebar.laplacian))
+        self.sidebar.canny.clicked.connect(lambda : self.apply_filters(self.sidebar.canny))
+        self.sidebar.median.clicked.connect(lambda : self.apply_filters(self.sidebar.median))
+        self.sidebar.sobel.clicked.connect(lambda : self.apply_filters(self.sidebar.sobel))
+        self.sidebar.blur.clicked.connect(lambda : self.apply_filters(self.sidebar.blur))
+        self.sidebar.bilateral.clicked.connect(lambda : self.apply_filters(self.sidebar.bilateral))
+        self.sidebar.grayscale.clicked.connect(lambda : self.apply_filters(self.sidebar.grayscale))
 
+        main_layout.addWidget(self.sidebar)
         main_layout.addLayout(image_layout)
 
     def open_image(self):
@@ -49,7 +63,11 @@ class MainWindow(QMainWindow):
                                               "Images (*.png, *.jpg, *.jpeg)")
         
         if (file):
-            print(file)
+            img = cv2.imread(file)
+            if img is None:
+                return
+            self.original_image = img
+            self.show_image(self.original_image, self.original_label)
 
     def open_video(self):
         file, _ = QFileDialog.getOpenFileName(self,
@@ -70,3 +88,63 @@ class MainWindow(QMainWindow):
         self.addToolBar(self.toolbar)
         self.toolbar.open_image.triggered.connect(self.open_image)
         self.toolbar.open_video.triggered.connect(self.open_video)
+
+    def show_image(self, image, label):
+        if (len(image.shape) == 2):
+            height, width = image.shape
+            bytes_per_line = width
+            qImage = QImage(image.data,
+                            width,
+                            height,
+                            bytes_per_line,
+                            QImage.Format_Grayscale8
+                            )
+            
+        else:
+            rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            height, width, channel = rgb.shape
+            bytes_per_line = channel * width
+            qImage = QImage(rgb.data,
+                            width,
+                            height,
+                            bytes_per_line,
+                            QImage.Format_RGB888)
+            
+        pixmap = QPixmap.fromImage(qImage)
+        pixmap = pixmap.scaled(label.size(),
+                               Qt.KeepAspectRatio,
+                               Qt.SmoothTransformation)
+        
+        label.setPixmap(pixmap)
+
+    def apply_filters(self, item):
+        if self.original_image is None:
+            return
+        
+        name = item.text()
+
+        if name == "Gaussian":
+            self.processed_image = ImageFilters.gaussian(self.original_image)
+
+        elif name == "Laplacian":
+            self.processed_image = ImageFilters.laplacian(self.original_image)
+
+        elif name == "Canny":
+            self.processed_image = ImageFilters.canny(self.original_image)
+
+        elif name == "Median":
+            self.processed_image = ImageFilters.median(self.original_image)
+
+        elif name == "Sobel":
+            self.processed_image = ImageFilters.sobel(self.original_image)
+
+        elif name == "Blur":
+            self.processed_image = ImageFilters.blur(self.original_image)
+
+        elif name == "Bilateral":
+            self.processed_image = ImageFilters.bilateral(self.original_image)
+        
+        elif name == "GrayScale":
+            self.processed_image = ImageFilters.grayscale(self.original_image)
+
+        self.show_image(self.processed_image, self.processed_label)
